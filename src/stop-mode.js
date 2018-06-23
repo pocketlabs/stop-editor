da395492d948e16dd233ad01f90e50745349f7fe
Kyle Shank <kyle.shank@gmail.com>
2018-06-23 09:59:37 -0400

    optional support

--- src/stop-mode.js
+++ src/stop-mode.js
@@ -7,7 +7,7 @@ ace.define('ace/mode/stop-mode',["require","exports","module","ace/lib/oop","ace
         var keywordMapper = this.createKeywordMapper({
             "keyword.control": "throws",
             "keyword.operator": "->|<-",
-            "keyword.other": "start|stop|async|include",
+            "keyword.other": "start|stop|async|include|optional",
             "storage.type": "enum|double|float|int32|int64|uint32|uint64|sint32|sint64|fixed32|fixed64|sfixed32|sfixed64|bool|string|bytes",
             "constant.language": "timeout"
   }, "identifier");
